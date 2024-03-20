746f7aec2bae66452c37c51a29d35cf8f92070d4
AhaanSehgal <ahaan@threely.io>
2024-03-20 16:44:29 +0530

    Final fixes

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,10 +1,11 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-//@ts-ignore
+
+// @ts-expect-error: Safe usage ensured by faker
 import faker from 'faker';
 
 const categories: string[] = [];
 for (let i = 0; i < 100; i++) {
-  categories.push(faker.commerce.department());
+  categories.push(faker?.commerce?.department());
 }
 
 export const postRouter = createTRPCRouter({
